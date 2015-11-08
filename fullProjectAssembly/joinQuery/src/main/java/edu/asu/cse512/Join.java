e442c3b4e75e21f20685386472e1190b4dad40a2
Jaya <jvutukur@asu.edu>
2015-11-07 18:19:24 -0700

    started Coding. Created polygon RDD

--- fullProjectAssembly/joinQuery/src/main/java/edu/asu/cse512/Join.java
+++ fullProjectAssembly/joinQuery/src/main/java/edu/asu/cse512/Join.java
@@ -1,9 +1,17 @@
 package edu.asu.cse512;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.FlatMapFunction;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+import java.util.ArrayList;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
 public class Join 
 {
 	/*
@@ -16,12 +24,85 @@ public class Join
 	*/
     public static void main( String[] args )
     {
-        // adding skelton
+        System.out.println("hello");
+ 
         //Initialize, need to remove existing in output file location.
+    	//String outputLocation = args[2];
+    	
     	
     	//Implement 
+    	String input1 = "/home/system/Desktop/TestData/JoinQueryTestData1.csv";  //args[0];
+    	String input2 = "/home/system/Desktop/TestData/JoinQueryTestData2.csv";  //args[1];
+    	SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+    	JavaSparkContext sc = new JavaSparkContext(conf);
+    	
+    	JavaRDD<String> input1RDD = sc.textFile(input1);
+    	JavaRDD<String> input2RDD = sc.textFile(input2);
+    	
+    	System.out.println(input1RDD.count());
+    	System.out.println(input2RDD.count());
+    	
+    	
+    	//new Funtion(inputType, returnType)
+    	JavaRDD<Geometry> input2Polygons = input2RDD.map(new Function<String,Geometry>(){
+    		public Geometry call(String coOrdinates){  Geometry g = getGeometry(coOrdinates); 
+    		return g;}
+    	});
+    		
+    	
+    	
+    	String inputType = "rectangle";//args[3];
+    	
+    	if(inputType.equals("point")){
+    		
+    	}
+    	else if(inputType.equals("rectangle")){
+    		JavaRDD<Geometry> input1Polygons = input1RDD.map(new Function<String,Geometry>(){
+        		public Geometry call(String coOrdinates){  Geometry g = getGeometry(coOrdinates); 
+        		return g;}
+        	});
+    		
+    		
+    		JavaRDD<List<Double>> output = input2Polygons.Map(new Function<Geometry ,List<Double>>{
+    			public List<Double> call(Geometry onePolygon){
+    				List<Double> eachLine = new ArrayList<Double>();
+    				
+    				
+    				return eachLine;
+    			}
+    		});
+    		
+    	}
+    
     	
     	//Output your result, you need to sort your result!!!
     	//And,Don't add a additional clean up step delete the new generated file...
+    	
     }
+    
+    
+    
+    public static Geometry getGeometry(String twocoOrdinates){
+    	String coOrdinates[] = twocoOrdinates.split(","); 
+		double x1=Double.parseDouble(coOrdinates[1]);
+		double y1=Double.parseDouble(coOrdinates[2]);
+		double x2=Double.parseDouble(coOrdinates[3]);
+		double y2=Double.parseDouble(coOrdinates[4]);
+		Polygon rect = new Polygon();
+		List<Double> x3y3x4y4 = rect.CalcultePolygon(x1, y1, x2, y2);
+		WKTReader reader = new WKTReader();
+		Geometry g = null;
+		try{
+			g=reader.read(String.format("POLYGON ((%f %f,%f %f,%f %f,%f %f,%f %f))",x1,y1,x3y3x4y4.get(0),x3y3x4y4.get(1),
+					x2,y2,x3y3x4y4.get(2),x3y3x4y4.get(3),x1,y1));
+			
+		}catch(ParseException e){
+			e.printStackTrace();    				
+		}
+		
+		return g;
+
+    }
+
 }
+
---
+++ fullProjectAssembly/joinQuery/src/main/java/edu/asu/cse512/Polygon.java
@@ -0,0 +1,124 @@
+package edu.asu.cse512;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.operation.overlay.snap.GeometrySnapper;
+import com.vividsolutions.jts.util.GeometricShapeFactory;
+
+public class Polygon {
+	
+	/* create rectangle reference 
+	 * http://math.stackexchange.com/questions/506785/given-two-diagonally-opposite
+	 * -points-on-a-square-how-to-calculate-the-other-two */
+	
+	private double x1;
+	private double x2;
+	private double y1;
+	private double y2;
+	private double xcentre;
+	private double ycentre;
+	private double xdia;
+	private double ydia;
+	private double   x3;
+	private double   y3;
+	private double   x4;
+	private double   y4;
+	public List<Double>  CalcultePolygon(double x1,double y1,double x2,double y2){
+		this.x1=x1;this.x2=x2;this.y1=y1;this.y2=y2;
+		System.out.println("here");
+		xcentre=(x1+x2)/2;
+		System.out.println("xc"+xcentre);
+		ycentre=(y1+y2)/2;
+		System.out.println("yc"+ycentre);
+		xdia=(x1-x2)/2;
+		System.out.println("xd"+xdia);
+		ydia=(y1-y2)/2;
+		System.out.println("yd"+ydia);
+		x3=x2;
+		y3=y1;
+		x4=x1;
+		y4=y2;
+		
+		System.out.println("x"+x3+"y"+y3+"x"+x4+"y"+y4);
+		List<Double> coordinate=Arrays.asList(x3,y3,x4,y4);
+		return coordinate;
+		
+	}
+	public double getX1() {
+		return x1;
+	}
+	public void setX1(double x1) {
+		this.x1 = x1;
+	}
+	public double getX2() {
+		return x2;
+	}
+	public void setX2(double x2) {
+		this.x2 = x2;
+	}
+	public double getY1() {
+		return y1;
+	}
+	public void setY1(double y1) {
+		this.y1 = y1;
+	}
+	public double getY2() {
+		return y2;
+	}
+	public void setY2(double y2) {
+		this.y2 = y2;
+	}
+	public double getXcentre() {
+		return xcentre;
+	}
+	public void setXcentre(double xcentre) {
+		this.xcentre = xcentre;
+	}
+	public double getYcentre() {
+		return ycentre;
+	}
+	public void setYcentre(double ycentre) {
+		this.ycentre = ycentre;
+	}
+	public double getXdia() {
+		return xdia;
+	}
+	public void setXdia(double xdia) {
+		this.xdia = xdia;
+	}
+	public double getYdia() {
+		return ydia;
+	}
+	public void setYdia(double ydia) {
+		this.ydia = ydia;
+	}
+	public double getX3() {
+		return x3;
+	}
+	public void setX3(double x3) {
+		this.x3 = x3;
+	}
+	public double getY3() {
+		return y3;
+	}
+	public void setY3(double y3) {
+		this.y3 = y3;
+	}
+	public double getX4() {
+		return x4;
+	}
+	public void setX4(double x4) {
+		this.x4 = x4;
+	}
+	public double getY4() {
+		return y4;
+	}
+	public void setY4(double y4) {
+		this.y4 = y4;
+	}
+}
+
